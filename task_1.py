c21fa067de50e08cb68dcdccea396cc5e2a433ee
Stepan250 <stepankolesnik250@gmail.com>
2024-02-09 23:45:17 +0300

    final

--- task_1.py
+++ task_1.py
@@ -8,12 +8,13 @@ for i in list:
 
 print(f"числа которые больше 20 по модулю: {list_2}")
 
+list_1 = [10, 21, -30, 120, 36, 3, -60, 35, 70]
 list_3 = []
-for i in range(0, len(list) - 2, 2):
-    number_1 = list[i]
-    number_2 = list[i + 2]
+for i in range(0, len(list_1) - 2, 2):
+    number_1 = list_1[i]
+    number_2 = list_1[i + 2]
     number_sum = number_1 + number_2
 
-    if number_sum < list[i + 1]:
-        list_3.append(list[i + 1])
+    if number_sum < list_1[i + 1]:
+        list_3.append(list_1[i + 1])
 print(f"среднии числа которое больше суммы соседних {list_3}")
