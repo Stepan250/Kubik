739fcaaf16f771f503aecfcf5c9d7a7f43e67de0
Stepan250 <stepankolesnik250@gmail.com>
2024-02-09 20:12:33 +0300

    first commit

--- task_1.py
+++ task_1.py
@@ -1,7 +1,19 @@
-list = [2981, 213, 32, 29, -32, -19, 3, 20]
+list = [21, 213, 32, 29, -32, -19, 3, 20, 170, 283, 321, 1, 451, 3123, 3213, 312, 4843, 4823, 20, 21]
+
 list_2 = []
 
 for i in list:
     if abs(i) > 20:
         list_2.append(i)
-print(list_2)
+
+print(f"числа которые больше 20 по модулю: {list_2}")
+
+list_3 = []
+for i in range(0, len(list) - 2, 2):
+    number_1 = list[i]
+    number_2 = list[i + 2]
+    number_sum = number_1 + number_2
+
+    if number_sum < list[i + 1]:
+        list_3.append(list[i + 1])
+print(f"среднии числа которое больше суммы соседних {list_3}")
