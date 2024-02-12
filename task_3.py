4845146df28d2f2b00b6e0ca77eb717f0662bd11
Stepan250 <stepankolesnik250@gmail.com>
2024-02-12 03:22:43 +0300

    final?

--- task_3.py
+++ task_3.py
@@ -3,6 +3,15 @@ alphabet_upper = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З',
                   'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 
                   'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
-for letter_upper in alphabet_upper:
-    letter_lower = letter_upper.lower()
-    print(f"{letter_upper, letter_lower}, ||, {letter_upper, letter_lower} ||, {letter_upper, letter_lower}")
+
+columns = 3
+letters_per_column = len(alphabet_upper) // columns
+
+for i in range(0, len(alphabet_upper), columns):
+    row = alphabet_upper[i:i + columns]
+    row_lower = [letter.lower() for letter in row]
+
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print(f"{row[0] + " " + row_lower[0]:^6} || {row[1] + " " + row_lower[1]:^6} || {row[2] + " " + row_lower[2]:^6}")
+
+print("^^^^^^^^^^^^^^^^^^^^^^^^^")
